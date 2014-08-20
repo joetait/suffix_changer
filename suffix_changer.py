2c8f88f48209848e52ba16ce6702e1a1bb7ac2d4
joetait <joetait@gmx.com>
2014-08-20 21:07:13 +0100

    Added initial comments for suffix_changer

---
+++ suffix_changer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+#Copyright 2014, Joe Tait, joetait "at" gmx "then dot" com
+
+#This file is part of suffix_changer
+
+#suffix_changer is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#suffix_changer is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with suffix_changer. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
